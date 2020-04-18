3e351aa15b8729d5170153c025e2b55cf57b767b
marceliotstein <marc.eliot.stein@gmail.com>
2020-04-18 16:30:13 -0400

    styling for Myths/Why/How section of front page

--- wpzoom-featured.php
+++ wpzoom-featured.php
@@ -1,38 +1,20 @@
 <div class="frontslider">
   <?php echo do_shortcode('[smartslider3 slider="8"]'); ?>
-  <?php if (is_front_page()) { ?>
-    <div class="wbw-mythswhyhow">
-      <h1>Please explore the real-world case for ending all war:</h1>
-      <table class="wbw-mythswhyhow-table"><tr>
-        <td class="wbw-mythswhyhow-td">
-          <h2>Myths About War</h2>
-          <div class="wbw-mwhlist">
-            &#8227; Myth: War Is Inevitable<br />
-            &#8227; Myth: War Is Necessary<br />
-            &#8227; Myth: War Is Beneficial<br />
-            &#8227; Myth: War Is Just<br />
-          </div>
-        </td>
-        <td class="wbw-mythswhyhow-td">
-          <h2>Why End War</h2>
-          <div class="wbw-mwhlist">
-            &#8227; War is Immoral<br />
-            &#8227; War Endangers Us<br />
-            &#8227; War Threatens Our Environment<br />
-            &#8227; War Erodes Liberties<br />
-            &#8227; War Improverishes Us<br />
-            &#8227; War Promotes Bigotry<br />
-            &#8227; We Need $2/Trillion A Year For Other Things
-          </div>
-        </td>
-        <td class="wbw-mythswhyhow-td">
-          <h2>How We Can End War</h2>
-          <div class="wbw-mwhlist">
-            &#8227; Divest From The War Machine<br />
-            &#8227; Close Bases 
-          </div>
-        </td>
-      </tr></table>
-    </div>
-  <?php } ?>
 </div>
+<?php if (is_front_page()) { ?>
+  <div class="wbw-mythswhyhow">
+    <h1>Please explore the real-world case for ending all war:</h1>
+    <h2>Myths About War</h2>
+    <div class="wbw-mwhlist">
+      <a href="#">Is War Inevitable?</a> <a href="#">Is War Necessary?</a> <a href="#">Is War Beneficial?</a> <a href="#">Is War Just?</a>
+    </div>
+    <h2>Why End War?</h2>
+    <div class="wbw-mwhlist">
+      <a href="#">War is Immoral</a>, <a href="#">War Endangers Us</a>, <a href="#">War Threatens Our Environment</a>, <a href="#">War Erodes Liberties</a>, <a href="#">War Improverishes Us</a>, <a href="#">War Promotes Bigotry</a>, <a href="#">We Need $2/Trillion A Year For Other Things</a>
+    </div>
+    <h2>How We Can End War</h2>
+    <div class="wbw-mwhlist">
+       <a href="#">Divest From The War Machine</a>, <a href="#">Close Bases</a>
+    </div>
+  </div>
+<?php } ?>
